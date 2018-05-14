a8b98feb965097a2782be498fadf4dcaaeb255a5
davidb <davidb@immensity.cms.waikato.ac.nz>
2018-05-14 16:55:01 +1200

    Upgrading of get script to do more setup

--- GET-PACKAGES-SOLR7.sh
+++ GET-PACKAGES-SOLR7.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-#echo "***"
-#echo "Zookeeper 3.4.9"
-#echo "***"
-#wget http://www-us.apache.org/dist/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz
-#echo "***"
-#echo ""
+get_zookeeper="y"
+zookeeper_ver=3.4.9
 
+get_solr="y"
 solr_ver=7.2.1
 
-echo ""
-echo "***"
-echo "Solr $solr_ver"
-echo "Note: There is a newer version out (7.3 released April 2018)"
-echo " => consider upgrading?"
-echo "***"
-echo ""
-wget http://archive.apache.org/dist/lucene/solr/$solr_ver/solr-$solr_ver.tgz
+if [ $get_zookeeper = "y" ] ; then
+    echo "***"
+    echo "Zookeeper $zookeeper_ver"
+    echo "***"
+    /bin/rm -f zookeeper-$zookeeper_ver.tar.gz zookeeper
+    wget http://archive.apache.org/dist/zookeeper/zookeeper-$zookeeper_ver/zookeeper-$zookeeper_ver.tar.gz \
+	&& tar xvzf zookeeper-$zookeeper_ver.tar.gz \
+	&& ln -s zookeeper-$zookeeper_ver zookeeper \
+	&& echo && echo "Created symbolic link 'zookeeper'"
+    echo "***"
+    echo ""
+fi
 
-echo "***"
-echo ""
+if [ $get_solr = "y" ] ; then
+    
+    echo ""
+    echo "***"
+    echo "Solr $solr_ver"
+    echo "Note: There is a newer version out (7.3 released April 2018)"
+    echo " => consider upgrading?"
+    echo "***"
+    echo ""
+    /bin/rm -f solr-$solr_ver.tgz solr7
+    wget http://archive.apache.org/dist/lucene/solr/$solr_ver/solr-$solr_ver.tgz \
+	&& tar xvzf solr-$solr_ver.tgz \
+	&& ln -s solr-$solr_ver solr7 \
+	&& echo && echo "Created symbolic link 'solr7'"
+    
+    echo "***"
+    echo ""
+fi
