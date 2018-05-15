18926e2eed88cec8d803b7d77041551dbcd97efc
davidb <davidb@immensity.cms.waikato.ac.nz>
2018-05-15 15:10:10 +1200

    Tweaks from testing

--- GET-PACKAGES-ZOOCREEPER.sh
+++ GET-PACKAGES-ZOOCREEPER.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+get_maven="y"
+maven_ver=3.5.3
+
+if [ $get_maven = "y" ] ; then
+    echo "***"
+    echo "Apache Maven $maven_ver"
+    echo "***"
+    /bin/rm -f apache-maven-$maven_ver-bin.tar.gz maven
+    wget http://archive.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-$maven_ver-bin.tar.gz \
+	&& tar xvzf apache-maven-$maven_ver-bin.tar.gz \
+	&& ln -s apache-maven-$maven_ver maven \
+	&& echo && echo "Created symbolic link 'maven'"
+    echo "***"
+    echo ""
+fi
+
+if [ -d "`pwd`/maven/" ] ; then
+    export PATH="`pwd`/maven/bin":$PATH
+    echo "Added maven to PATH"
+fi
+
 /bin/rm zoocreeper-master.zip
 wget https://github.com/boundary/zoocreeper/archive/master.zip -O zoocreeper-master.zip \
     && unzip zoocreeper-master.zip \
--- setup/setup-zookeeper.bash
+++ setup/setup-zookeeper.bash
@@ -4,7 +4,7 @@ export ZOOKEEPER_HOME="$HTRC_EF_PACKAGE_HOME/zookeeper"
 export ZOOCREEPER_HOME="$HTRC_EF_PACKAGE_HOME/zoocreeper-master"
 
 export PATH="$ZOOKEEPER_HOME/bin:$PATH"
-export PATH="$ZOOCREEPER_HOME/bin:$PATH"
+export PATH="$ZOOCREEPER_HOME:$PATH"
 
 if [ "$short_hostname" = "gsliscluster1" ] ; then
   echo "* Added in Zookeeper nad Zoocreeper into PATH"
