ba597f4750edfc3976ca22ac27c0f783cbf4148a
David Bainbridge <dbbridge@mail.lis.illinois.edu>
2018-05-23 18:11:02 -0500

    More portable

--- setup/setup-spark.bash
+++ setup/setup-spark.bash
@@ -1,7 +1,8 @@
 
 export SPARK_HOME="$HTRC_EF_PACKAGE_HOME/spark"
 export PATH="$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH"
-if [ "$short_hostname" = "gsliscluster1" ] ; then
+#if [ "$short_hostname" = "gsliscluster1" ] ; then
+if [ "x$do_echo" != "x" ] && [ "$do_echo" = "1" ] ; then     
   echo "* Added in Spark's bin and sbin into PATH"
 fi
 
