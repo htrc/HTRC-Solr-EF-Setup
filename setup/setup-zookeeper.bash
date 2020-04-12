46b17bd1c80bf5860fb685061849d459df4a34bd
David Bainbridge <davidb@waikato.ac.nz>
2020-04-12 06:10:21 -0500

    Adjustments so separate Zookeepers installs can be operational for solr7 and solr8

--- setup/setup-zookeeper.bash
+++ setup/setup-zookeeper.bash
@@ -3,11 +3,16 @@
 export ZOOKEEPER_HOME="$HTRC_EF_PACKAGE_HOME/zookeeper"
 export ZOOCREEPER_HOME="$HTRC_EF_PACKAGE_HOME/zoocreeper-master"
 
-export PATH="$ZOOKEEPER_HOME/bin:$PATH"
-export PATH="$ZOOCREEPER_HOME:$PATH"
+# htrc-ef-zookeeper-... scripts now run by specifying full-path rather than finding it via PATH
+# Using full paths allows for more than one version of zookeeper to be installed on the filesystem
+
+#export PATH="$ZOOKEEPER_HOME/bin:$PATH"
+#export PATH="$ZOOCREEPER_HOME:$PATH"
 
 if [ "$short_hostname" = "gsliscluster1" ] ; then
-  echo "* Added in Zookeeper and Zoocreeper into PATH"
+#  echo "* Added in Zookeeper and Zoocreeper into PATH"
+  echo "* Set ZOOKEEPER_HOME to be: $ZOOKEEPER_HOME"
+  echo "* Set ZOOCREEPER_HOME (for backup dumps) to be: $ZOOCREEPER_HOME"
 fi
 
 
