8b7401d59ebe7e3b6ca47bde24c6b67a62110c91
David Bainbridge <davidb@waikato.ac.nz>
2020-04-13 00:41:51 -0500

    Only want to run TOPUP if password been setup in realm.properties

--- TOPUP-SOLR8-JETTY.sh
+++ TOPUP-SOLR8-JETTY.sh
@@ -6,6 +6,17 @@
 
 # Also add in realm authentication for admin area
 
+if [ ! -f conf/jetty-solr8/realm.properties ] ; then
+    echo "****" >&2
+    echo "* Failed to detect:" >&2
+    echo "*   conf/jetty-solr8/realm.properties" >&2
+    echo "* This means no password has been set yet for the Solr/Jetty Admin UI" >&2
+    echo "* This can be done by running:" >&2
+    echo "*   ./SOLR8-JETTY-SET-PASSWORD.sh" >&2
+    echo "****" >&2
+    exit -1
+fi
+
 echo ""
 echo "Adding in cross-origin settings and realm authentication to Jetty config files"
 echo ""
