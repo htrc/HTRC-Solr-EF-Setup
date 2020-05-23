9a54ab4809a8f89f93293593a4e238eda6efe3d9
David Bainbridge <davidb@waikato.ac.nz>
2020-05-22 21:57:25 -0500

    Changed to using local dir names, so SETUP8.bash doesn't need to have been run to do the necessary installation steps

--- SOLR8-TOPUP-CONFIGSETS.sh
+++ SOLR8-TOPUP-CONFIGSETS.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-solr8_configsets="$SOLR8_TOP_LEVEL_HOME/server/solr/configsets"
+#if [ "x$SOLR8_TOP_LEVEL_HOME" = "x" ] ; then
+#    echo "****" >&2
+#    echo "* Environment variable SOLR8_TOP_LEVEL_HOME not set.  Have you sourced SETUP8.bash?" >&2    
+#    echo "****" >&2
+#    exit 1
+#fi
+
+#solr8_configsets="$SOLR8_TOP_LEVEL_HOME/server/solr/configsets"
+solr8_configsets="solr8/server/solr/configsets"
 
 echo "Copying conf/htrc_configs-docvals-841 in Solr8 configtests directory: $solr8_configsets"
 /bin/cp -r conf/htrc-configs-docvals-841 "$solr8_configsets/htrc-configs-docvals"
