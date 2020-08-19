5b64fc3b7d982bbca066cd6c4d790797c18c99ea
David Bainbridge <davidb@waikato.ac.nz>
2020-08-19 01:40:29 -0500

    Further developed

--- SOLR8-CHOWN-CHGRP-CHMOD.sh
+++ SOLR8-CHOWN-CHGRP-CHMOD.sh
@@ -2,11 +2,16 @@
 
 
 
-
 systemctl_user=${1:-www-data}
-systemctl_group=${2:-htrc}
 
-solr8_shard_dirs='/disk*/htrc-ef-solr8-shards/'
+
+if [ -d "/disk1/htrc-ef-solr8-shards/" ] ; then
+    systemctl_group=${2:-htrc}
+    solr8_shard_dirs='/disk*/htrc-ef-solr8-shards/'
+else
+    systemctl_group=${2:-htrc-solr}
+    solr8_shard_dirs='/solr-data/node*/htrc-ef-solr8-shards/'
+fi
 
 echo "****"
 echo "* Changing user:group ownership to (and adding group-write permission for):"
@@ -16,8 +21,15 @@ echo "*  $solr8_shard_dirs"
 echo "****"
 
 
+echo ""
+echo "Top-level full dir listing:"
+echo ""
 
-sudo chown -R $systemctl_user  $solr8_shard_dirs
+sudo chown -R $systemctl_user   $solr8_shard_dirs
 sudo chgrp -R $systemctl_group  $solr8_shard_dirs
+sudo chmod -R g+s $solr8_shard_dirs
 sudo chmod -R g+w $solr8_shard_dirs
+
+ls -ld $solr8_shard_dirs
+echo ""
 
