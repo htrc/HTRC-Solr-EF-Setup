8bacff50305e8e69986625494b24779504148842
David Bainbridge <davidb@waikato.ac.nz>
2020-11-10 22:20:14 -0600

    Useful script to run solr/jetty processes on a given server as the www-data user

---
+++ WWW-DATA-HTRC-EF-SOLR8-START-ON-THIS-HOST.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo -u www-data -- bash -c "source ./SETUP8.bash && htrc-ef-solr8-start-on-this-host.sh"
---
+++ WWW-DATA-HTRC-EF-SOLR8-STOP-ON-THIS-HOST.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo -u www-data -- bash -c "source ./SETUP8.bash && htrc-ef-solr8-stop-on-this-host.sh"
