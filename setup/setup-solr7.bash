304c5ff246bd4d2bb6487af855c114bcffdfa09d
David Bainbridge <davidb@waikato.ac.nz>
2021-12-16 17:09:04 -0600

    Update to address Log4J vulnerability CVE-2021-44228

--- setup/setup-solr7.bash
+++ setup/setup-solr7.bash
@@ -1,5 +1,18 @@
 
 export SOLR7_TOP_LEVEL_HOME="$HTRC_EF_PACKAGE_HOME/solr7"
+
+# In response to:
+#   https://solr.apache.org/security.html#apache-solr-affected-by-apache-log4j-cve-2021-44228
+
+## Explicitly specify directory where solr.in.sh is located
+## which 'solr' sript will read in to set any environment variables
+#export SOLR_INCLUDE="$SOLR7_TOP_LEVEL_HOME/bin"
+
+# Currently favour the more direct route to setting this environment variable, as easier
+# to roll out, once committed in github to repository
+
+export SOLR_OPTS="$SOLR_OPTS -Dlog4j2.formatMsgNoLookups=true"
+
 export SOLR7_HOME="$SOLR_TOP_LEVEL_HOME/server/solr"
 #export SOLR_PID_DIR="$SOLR_HOME"
 
--- setup/setup-solr8.bash
+++ setup/setup-solr8.bash
@@ -1,5 +1,19 @@
 
 export SOLR8_TOP_LEVEL_HOME="$HTRC_EF_PACKAGE_HOME/solr8"
+
+# In response to:
+#   https://solr.apache.org/security.html#apache-solr-affected-by-apache-log4j-cve-2021-44228
+
+## Explicitly specify directory where solr.in.sh is located
+## which 'solr' sript will read in to set any environment variables
+#export SOLR_INCLUDE="$SOLR7_TOP_LEVEL_HOME/bin"
+
+# Currently favour the more direct route to setting this environment variable, as easier
+# to roll out, once committed in github to repository
+
+export SOLR_OPTS="$SOLR_OPTS -Dlog4j2.formatMsgNoLookups=true"
+
+
 export SOLR8_HOME="$SOLR8_TOP_LEVEL_HOME/server/solr"
 #export SOLR8_PID_DIR="$SOLR8_HOME"
 
